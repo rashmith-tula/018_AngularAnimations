a3129da28a289833e8d7c1c1794683ea52532ea4
Rashmith <rashmith.tula@gmail.com>
2019-03-26 13:33:18 +0530

    Modularized html code into their respective templates

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ToDoService } from '../todo.service';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { state, style, trigger, transition, animate } from '@angular/animations';
+import { state, style, trigger, transition, animate, group } from '@angular/animations';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css'],
   animations: [
-      trigger('onAdd', [
-        state('normal', style({opacity: 1, 'transform' : 'translateX(0)'})),
-        transition('void => *', [
-          style({opacity: 0, 'transform': 'translateX(-100px)'}),
-          animate(300)
-        ]),
-        transition('* => void', [
-          animate(300, style({
+    trigger('onAdd', [
+      state('normal', style({ opacity: 1, 'transform': 'translateX(0)' })),
+      transition('void => *', [
+        style({ opacity: 0, 'transform': 'translateX(-200px)' }),
+        animate(500)
+      ]),
+      transition('* => void', [
+        animate(300, style({ backgroundColor: 'red' })),
+        group([
+          animate(300, style({'font-weight': 'bold'})),
+          animate(500, style({
             opacity: 0,
-            'transform': 'translateX(100px)'
+            'transform': 'translateX(200px)'
           }))
         ])
+
       ])
+    ])
   ]
 })
 export class BodyComponent implements OnInit {
@@ -35,6 +40,7 @@ export class BodyComponent implements OnInit {
 
   onDelete(id: number) {
     this.todoService.deleteTodo(id);
+    // this.todos.splice(id, 1);
   }
 
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { faPlusCircle, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import { NgForm } from '@angular/forms';
-import { state, style, trigger, transition, animate, group } from '@angular/animations';
 import { ToDoService } from '../todo.service';
 
 
@@ -9,31 +8,10 @@ import { ToDoService } from '../todo.service';
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
-  animations: [
-    trigger('onAdd', [
-      state('normal', style({ opacity: 1, 'transform': 'translateX(0)' })),
-      transition('void => *', [
-        style({ opacity: 0, 'transform': 'translateX(-200px)' }),
-        animate(500)
-      ]),
-      transition('* => void', [
-        animate(300, style({ backgroundColor: 'red' })),
-        group([
-          animate(300, style({'font-weight': 'bold'})),
-          animate(500, style({
-            opacity: 0,
-            'transform': 'translateX(200px)'
-          }))
-        ])
-
-      ])
-    ])
-  ]
 })
 export class FormComponent implements OnInit {
   todos: any[] = [];
   faPlusCircle = faPlusCircle;
-  faTrash = faTrash;
   invalid = false;
 
   constructor(private todoService: ToDoService) { }
@@ -59,11 +37,6 @@ export class FormComponent implements OnInit {
     } else {
        this.invalid = true;
     }
-  }
-
-  onDelete(id: number) {
-    this.todoService.deleteTodo(id);
-    // this.todos.splice(id, 1);
   }
 
 }
